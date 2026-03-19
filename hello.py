facf9c96f52dcea8e3ef2d26e2afb2f1adfd55ef
Kwanusu <kwanusujoseph@gmail.com>
2026-03-19 12:07:00 +0300

    First commit

---
+++ hello.py
@@ -0,0 +1,54 @@
+# This function prints the output of the parenthesis in the terminal
+print('Hello World! Welcome to python programming')
+# This is a comment
+# this is a multiline comment
+def add_num():
+    """ a function that takes numbers and adds them together"""
+    pass
+
+x = 5
+y = '5'
+# z = 'somestuff'
+z = "somestuff"
+pi = 3.14
+profile = {'name': 'Joseph', 'occupation': 'software developer'}
+veges = ('cabbage', 'kales', 'spinach')
+is_adult = True
+is_married = False
+this_is_a_very_long_variable = ''# snake case
+a = 35
+worked_hours = 35 # mnemonic
+hours = 7 
+
+
+
+area = pi * x * x
+print(area)
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(pi))
+print(type(is_adult))
+print(type(profile))
+print(type(veges))
+
+
+# Variables
+sum = 2 / 2
+div = 10 % 3
+expo = 10 ** 5
+print(sum)
+print(div)
+print(expo)
+
+check_val = 5 == 3 # false
+check = 5 != 3 # not equal
+check1 = 2 >= 3
+check2 = 5 <= 3
+print(check_val)
+print(check)
+print(check1)
+print(check2)
+
+name = input()
+print(name)
---
+++ user_functions.py
@@ -0,0 +1,10 @@
+def function_name():# defining the function
+    pass # A function to calculate mean marks 
+
+def say_hello(name):
+    print(f"Hello {name}!")
+say_hello("Grace") # Calling the function  
+
+def sum(a, b):
+    return a + b
+print(sum(23, 45))
